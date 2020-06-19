3dc28ee9aa26597a99c685bde9d23cc0f78053f5
Alessio Glorioso <alessioglorioso@MBP-de-Antonio.lan>
2020-06-19 15:07:24 +0200

    debut diff cont

---
+++ srcs/Mysql/srcs/sql_init.sh
@@ -0,0 +1,6 @@
+mysql -u root -e "CREATE DATABASE wordpress;"
+mysql -u root -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';"
+mysql -u root -e "CREATE USER 'aglorios'@'localhost' IDENTIFIED BY 'mdp123';"
+mysql -u root -e "CREATE USER 'invite'@'localhost' IDENTIFIED BY 'invite';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'admin'@'localhost';"
+mysql -u root -e "FLUSH PRIVILEGES;"
