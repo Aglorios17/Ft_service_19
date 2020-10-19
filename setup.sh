4107f754eb020441d49947c3998317540a4bd346
Alessio Glorioso <aglorios@MBP-de-Alessio.lan>
2020-10-19 19:00:53 +0200

    metallb ip bug

--- setup.sh
+++ setup.sh
@@ -2,15 +2,17 @@ echo "verif..."
 minikube stop
 minikube delete --all
 
-echo "Brew Minikube..."
-#brew minikube
+#echo "Brew Minikube..."
+#brew install minikube
 
 echo "Minikube start..."
-minikube start --extra-config=apiserver.service-node-port-range=1-6000
+minikube start --driver=virtualbox --memory='3000' --disk-size 5000MB
+#minikube start
 
-#echo "addons..."
-#minikube addons enable ingress
-#minikube addons enable dashboard
+
+echo "addons..."
+minikube addons enable metallb
+minikube addons enable dashboard
 
 echo "new cluster..."
 minikube kubectl -- get po -A
@@ -18,23 +20,27 @@ minikube kubectl -- get po -A
 echo "Minikube dashboard..."
 minikube dashboard &
 
+echo "metallb..."
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml #create namespace/metallb-system
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml #install metallb for load balancer, on cluster under metallb-system
+#On first install only, generate random bytes based secretkey
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey=“$(openssl rand -base64 128)”
+
+kubectl apply -f srcs/metallb.yaml
+
 eval $(minikube docker-env)
 
 echo "Nginx..."
 docker build -t mynginx	./srcs/nginx/
-docker build -t mywordpress	./srcs/Wordpress/
-docker build -t myphpmyadmin ./srcs/Phpmyadmin/
-docker build -t myftps ./srcs/FTPS/
+#echo "Wordpress..."
+#docker build -t mywordpress	./srcs/Wordpress/
+#echo "phpmyadmin..."
+#docker build -t myphpmyadmin ./srcs/Phpmyadmin/
+#echo "ftps..."
+#docker build -t myftps ./srcs/FTPS/
+
 
 echo "deploy..."
-#kubectl	-- get pods
-kubectl run nginx-deployement --image=mynginx:latest --image-pull-policy=Never
-kubectl run wordpress-deployement --image=mywordpress:latest --image-pull-policy=Never
-kubectl run phpmyadmin-deployement --image=myphpmyadmin:latest --image-pull-policy=Never
-kubectl run ftps-deployement --image=myftps:latest --image-pull-policy=Never
+kubectl create -f srcs/nginx/nginx.yaml
 
-kubectl expose deployment nginx-deployement --type=NodePort --port=80 --target-port=443
-kubectl expose deployment wordpress-deployement --type=NodePort --port=5050
-kubectl expose deployment phpmyadmin-deployement --type=NodePort --port=5000
-kubectl expose deployment ftps-deployement --type=NodePort --port=21
-minikube service list
+kubectl get svc
