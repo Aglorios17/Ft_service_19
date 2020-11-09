7f58a08a30a62854430b14e0e7c2cec5da457e1e
Alessio Glorioso <aglorios@MBP-de-Alessio.lan>
2020-11-09 18:18:16 +0100

    grafana/influxdb debut

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 echo "verif..."
-minikube stop
-minikube delete --all
+#minikube stop
+#minikube delete --all
 
 #echo "Brew Minikube..."
 #brew install minikube
@@ -33,14 +33,19 @@ echo "phpmyadmin..."
 docker build -t myphpmyadmin ./srcs/Phpmyadmin/
 echo "mysql..."
 docker build -t mysql ./srcs/Mysql/
+echo "influxdb..."
+docker build -t myinfluxdb ./srcs/influxdb/
+echo "Grafana..."
+docker build -t mygrafana ./srcs/Grafana/
 #echo "ftps..."
 #docker build -t myftps ./srcs/FTPS/
 
-
 echo "deploy..."
 kubectl apply -f srcs/Mysql/sql.yaml
+kubectl apply -f srcs/influxdb/influx.yaml
 kubectl apply -f srcs/nginx/nginx.yaml
 kubectl apply -f srcs/Phpmyadmin/phpmyadmin.yaml
 kubectl apply -f srcs/Wordpress/wordpress.yaml
+kubectl apply -f srcs/Grafana/grafana.yaml
 
 kubectl get svc
